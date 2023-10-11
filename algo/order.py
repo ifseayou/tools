f8106ca457d09d9e38aa9bab27598e3ede99ee85
ifseayou <ifseayou@gmail.com>
2023-10-11 17:11:26 +0800

    add insert sort and merge sort

--- algo/order.py
+++ algo/order.py
@@ -31,16 +31,83 @@ def selectSort(arr):
                 minIdx = j
         arr[i],arr[minIdx] = arr[minIdx],arr[i]
 
+def insertSort(arr):
+    if arr == None or len(arr) <= 1:
+        return
+
+    n = len(arr)
+    for i in range(1,n):
+        for j in range(i,0,-1):
+            if arr[j] <  arr[j-1]:
+                arr[j],arr[j-1] = arr[j-1], arr[j]
+
+
+def mergeSort(arr):
+    if arr == None or len(arr) <= 1:
+        return
+
+    n = len(arr)
+
+    sortProcess(arr,0,n - 1)
+
+
+def sortProcess(arr,L,R):
+    if L >= R:
+        return
+    
+    middle = int(L + (R - L) / 2)
+    sortProcess(arr,L,middle)
+    sortProcess(arr,middle+1,R)
+    merge(arr,L,middle,R)
+    
+
+def merge(arr,L,middle,R):
+    help = [0] * (R - L + 1)
+    p1 = L
+    p2 = middle + 1
+    i = 0
+    while  p1 <= middle and p2 <= R:
+        if arr[p1] <= arr[p2]:
+            print(p1)
+            help[i] = arr[p1]
+            p1 = p1 + 1
+            i = i + 1
+        else:
+            help[i] = arr[p2]
+            p2 = p2 +1
+            i = i + 1 
+    
+    while p1 <= middle:
+        help[i] = arr[p1]
+        i = i + 1
+        p1 = p1 + 1
+
+    while p2 <= R:
+        help[i] = arr[p2]
+        i = i + 1
+        p2 = p2 + 1
+
+    for j in range(len(help)):
+        arr[L+j] = help[j]
+
+
 
 
 if __name__ == '__main__':
-    arr = [1,4,8,2,3,0]
+    arr = [1,4,8,0,2,3,0]
 
-    # bubbleSort(arr)    
+    # test the bubble sort
     # bubbleSort(arr)
 
     # test the select sort 
-    selectSort(arr)
+    # selectSort(arr)
+
+    # test the insert sort
+    # insertSort(arr)
+
+    # test the merge sort
+    mergeSort(arr)
+
 
     for i in arr :
         print(i,end="\t")    
---
+++ zfile/edit.py
@@ -0,0 +1,6 @@
+
+if __name__ == '__main__':
+    print('dow d')
+    for i in range(10,5,-1):
+        print(i)
+    print('idd df ')
