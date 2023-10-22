243c3221f8b09f591d23aee05bd605cb2e33b5de
ifseayou <ifseayou@gmail.com>
2023-10-22 15:56:28 +0800

    get cycle node

--- 1.sql
+++ 1.sql
@@ -1,43 +1,37 @@
-insert overwrite table dw.fy_month_shop_stat_df partition (month_id = '2023-10-18')
-select current_timestamp()                           as etl_time       -- etl时间
+5 -> 4 -> 3 -> 2 -> 1 - > 3
 
-     , uuid() as row_id         -- 编号
+第一次：
+s = 5
+f = 5 
+  
+
+第二次
+s = 4 
+f = 3
 
 
-     , if(t2.order_month is not null,t2.order_month,t3.order_month) as order_month    -- 销售月份
+第三次
+s = 3
+f = 1
+
+第四次
+s = 2
+f = 2
+
+新的循环：
+
+第五次
+s = 1
+f = 1  -- 入环节点 = f.next
+
+第六次
+s = 4
+f = 3
 
 
-     
-     , t1.f_company_name                             as f_company_name -- 合同主体-公司名称
-     
-     , if(t2.order_month is not null,t2.con_shop_code,t3.con_shop_code)  as con_shop_code  -- 铺位号-商铺编码
 
-     , t2.con_shop_name                              as con_shop_name  -- 商铺名称
-     , t1.customer_name                              as customer_name  -- 商户名-商户名称
-     , t1.brand_name                                 as brand_name     -- 品牌名-品牌名称
 
-     , t2.off_shop_name                              as off_shop_name  -- 线下店铺ID
-     , t2.off_shop_id                                as off_shop_id    -- 线下店铺名称
+对于一个单链表， 判断这个链表是否为带环链表，如果是 返回入环节点，Java 实现
 
-     , t2.shop_id                                    as on_shop_id     -- 线上店铺ID
-     , t2.shop_name                                  as on_shop_name   -- 线上店铺名称
-     , t2.platform_name                              as platform_name  -- 平台
-
-     , t1.store_id                                   as store_id       -- 门店 ID
-     , t1.los_store_id                               as los_store_id   -- 租赁租户 ID
-
-     , t2.on_pay_gmv                                 as on_pay_gmv     -- 本月线上付款GMV
-     , t2.on_deal_gmv                                as on_deal_gmv    -- 本月线上成交GMV
-     , t2.on_rfd_gmv                                 as on_rfd_gmv     -- 本月线上退款额
-     , t2.on_rfd_rate                                as on_rfd_rate    -- 本月线下退款率
-
-     , t3.off_pay_gmv                                as off_pay_gmv    --  本月线下销售额
-     , t3.off_deal_gmv                               as off_deal_gmv   --  本月线下结算额
-from `tmp.dw_fy_month_shop_stat_df_02` t1   -- 亿高商铺
-left join `tmp.dw_fy_month_shop_stat_df_04`      t2 -- 线上店铺
-          on t2.con_shop_code = t1.con_shop_code
-
-full join `tmp.dw_fy_month_shop_stat_df_03` t3 -- 数衍 线下数据
-          on t2.order_month = t3.order_month
-              and t2.con_shop_code = t3.con_shop_code
-;
+比如输入为： 5 -> 4 -> 3 -> 2 -> 1 -> 3 
+返回为：3
--- algo/linkList.py
+++ algo/linkList.py
@@ -4,7 +4,7 @@
 @Time    :   2023/10/20 16:07:06
 @Author  :   benchen
 @Contact :   benchen@yowant.com
-@Desc    :   反转链表
+@Desc    :   反转链表 &  寻找带环链表的入环节点
 '''
 
 class Node:
@@ -20,7 +20,7 @@ def printLinkList(head):
         else:
             print(cur.val)
         cur = cur.next
-        
+# 反转链表        
 def reverseLinkList(head):
     pre = None
     cur = head
@@ -31,13 +31,34 @@ def reverseLinkList(head):
         head = cur    
     return pre
 
+# 返回带环节点的入环节点
+def getCycleNode(head):
+    if head == None or head.next == None:
+        return None
+
+    slow = head
+    fast = head
+    while fast != None and fast.next != None:
+        slow = slow.next
+        fast = fast.next.next
+        if fast == slow:
+            p = head
+            while p != slow:
+                p = p.next
+                slow = slow.next
+            return p
+    return None
+    
+
 if __name__ == '__main__':
-    head = Node(1,Node(2,Node(3,Node(4,Node(5)))))
+    n1 = Node(1)
+    n2 = Node(2,n1)
+    n3 = Node(3,n2)
+    n4 = Node(4,n3)
+    head = Node(5,n4)
 
-    printLinkList(head)
-
-    # 反转链表
-    head = reverseLinkList(head)
-
-    printLinkList(head)
-
+    # printLinkList(n5)
+    n1.next = n2
+    p1 = getCycleNode(head)
+    if p1 != None:
+        print(p1.val)
